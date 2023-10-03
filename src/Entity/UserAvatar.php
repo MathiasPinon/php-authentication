d72b34478325200f403377d00463ef7ccbd9e396
Mathias Pinon <mathias.pinon@etudiant.univ-reims.fr>
2023-10-03 09:22:58 +0200

    Création de la méthode findById()

--- src/Entity/UserAvatar.php
+++ src/Entity/UserAvatar.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Entity;
 
 use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
 
 class UserAvatar
 {
@@ -16,15 +17,23 @@ class UserAvatar
         return $this->avatar;
     }
 
-    public static function findById(int $userId){
+    public static function findById(int $userId)
+    {
         $sql = MyPdo::getInstance()->prepare(
             <<<SQL
             SELECT id , avatar
             FROM User 
             WHERE id = :id 
 SQL
-
         );
-        $sql->setFetchMode(PDO:)
+        $sql->execute([':id' => $userId]);
+
+        $sql->setFetchMode(\PDO::FETCH_CLASS, UserAvatar::class)
+        ;
+        $user = $sql->fetch();
+        if($user === false ){
+            throw new EntityNotFoundException("Il n'y a pas de ligne séléctionnée");
+        }
+        return $user ;
     }
 }
